8c1969143bfe4e26e9eb0331737dd926490af740
sds <sds>
1997-10-17 20:34:51 +0000

    Added plot-lists-arg.

--- src/cllib/gnuplot.lisp
+++ src/cllib/gnuplot.lisp
@@ -1,10 +1,13 @@
-;;; File: <gnuplot.lisp - 1997-10-15 Wed 11:43:41 EDT - sds@WINTERMUTE.eagle>
+;;; File: <gnuplot.lisp - 1997-10-17 Fri 16:34:41 EDT - sds@WINTERMUTE.eagle>
 ;;;
-;;; Gnuplot
+;;; Gnuplot interface
 ;;;
-;;; $Id: gnuplot.lisp,v 1.4 1997/10/15 15:44:17 sds Exp $
+;;; $Id: gnuplot.lisp,v 1.5 1997/10/17 20:34:51 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/gnuplot.lisp,v $
 ;;; $Log: gnuplot.lisp,v $
+;;; Revision 1.5  1997/10/17 20:34:51  sds
+;;; Added plot-lists-arg.
+;;;
 ;;; Revision 1.4  1997/10/15 15:44:17  sds
 ;;; Added plot-lists.
 ;;; Made plot-dated-lists plot exponential moving averages.
@@ -41,7 +44,7 @@ EMA is the list of parameters for Exponential Moving Averages."
 		   ((null begd) (dl-beg-date (car dls)))
 		   (t (parse-date begd)))
 	endd (cond ((date-p endd) endd)
-		   ((null endd) (dl-beg-date (car dls)))
+		   ((null endd) (dl-end-date (car dls)))
 		   (t (parse-date endd))))
   (unless dls (error "nothing to plot for `~a'~%" title))
   (with-open-file (str *gnuplot-file* :direction :output :if-exists :supersede)
@@ -102,6 +105,30 @@ plot~{ '-' using 1:2 title '~a'~^,~}~%"
 	(do ((ll (cdr ls) (cdr ll)) (ix 0 (1+ ix)))
 	    ((= ix depth) (format str "e~%"))
 	  (format str "~f~10t~f~%" ix (funcall val ll))))))
+  (format t "Prepared file `~a'. Type \"load '~a'\" at the gnuplot prompt.~%"
+	  *gnuplot-file* *gnuplot-file*))
+
+(defun plot-lists-arg (lss &key (key #'identity) (title "Plot") (xlabel "nums")
+		       (ylabel "value") (data-style "linespoints") rel)
+  "Plot the given lists of numbers.
+LSS is a list of lists, car of each list is the title, cdr is the list
+of conses of abscissas and ordinatas."
+  (declare (list lss))
+  (let ((beg (apply #'min (mapcar #'caadr lss)))
+	(end (apply #'max (mapcar (compose #'caar #'last) lss))))
+    (with-open-file (str *gnuplot-file* :direction :output
+		     :if-exists :supersede)
+      (format str "set xdata~%set xlabel '~a'~%set ylabel '~a'
+set xrange [~d:~d]~%set data style ~a~%set title '~a'
+plot~{ '-' using 1:2 title '~a'~^,~}~%"
+	      xlabel ylabel beg end data-style title (mapcar #'car lss))
+      (let* (bv (val (if rel (lambda (kk) (/ kk bv)) #'identity)))
+	(dolist (ls lss)
+	  (setq bv (funcall key (cadr ls)))
+	  (do ((ll (cdr ls) (cdr ll)) kk)
+	      ((null ll) (format str "e~%"))
+	    (setq kk (funcall key (car ll)))
+	    (format str "~f~10t~f~%" (car kk) (funcall val (cdr kk))))))))
   (format t "Prepared file `~a'. Type \"load '~a'\" at the gnuplot prompt.~%"
 	  *gnuplot-file* *gnuplot-file*))
 

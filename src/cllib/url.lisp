72d740330eaa09c71156520e13864cb80d7c659e
sds <sds>
1999-04-16 16:01:18 +0000

    (with-tag): added `value' key; better default for `terpri'. (with-open-html): added `head', `comment' and `footer' keys; fixed `doctype' key. (directory-index): added `&rest opts' for comment. use `value' key when calling `with-tag'.

--- src/cllib/url.lisp
+++ src/cllib/url.lisp
@@ -1,4 +1,4 @@
-;;; File: <url.lisp - 1999-04-11 Sun 15:56:35 EDT sds@eho.eaglets.com>
+;;; File: <url.lisp - 1999-04-16 Fri 11:54:05 EDT sds@eho.eaglets.com>
 ;;;
 ;;; Url.lisp - handle url's and parse HTTP
 ;;;
@@ -9,9 +9,16 @@
 ;;; conditions with the source code. See <URL:http://www.gnu.org>
 ;;; for details and precise copyright document.
 ;;;
-;;; $Id: url.lisp,v 1.22 1999/04/11 19:58:00 sds Exp $
+;;; $Id: url.lisp,v 1.23 1999/04/16 16:01:18 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/url.lisp,v $
 ;;; $Log: url.lisp,v $
+;;; Revision 1.23  1999/04/16 16:01:18  sds
+;;; (with-tag): added `value' key; better default for `terpri'.
+;;; (with-open-html): added `head', `comment' and `footer' keys;
+;;; fixed `doctype' key.
+;;; (directory-index): added `&rest opts' for comment.
+;;; use `value' key when calling `with-tag'.
+;;;
 ;;; Revision 1.22  1999/04/11 19:58:00  sds
 ;;; Added `*html-output*' and `with-tag'.
 ;;; (with-open-html): bind `*html-output*'.  use `with-tag'.
@@ -464,6 +471,7 @@ The argument can be:
    (mesg :type simple-string :reader net-mesg :initarg :mesg :initform "")
    (args :type list :reader net-args :initarg :args :initform nil))
   (:report (lambda (cc out)
+             (declare (stream out))
              (format out "[~s] ~s:~d~@[ ~?~]"
                      (net-proc cc) (net-host cc) (net-port cc)
                      (when (slot-boundp cc 'mesg) (net-mesg cc))
@@ -472,6 +480,7 @@ The argument can be:
 (define-condition timeout (network)
   ((time :type (real 0) :reader timeout-time :initarg :time))
   (:report (lambda (cc out)
+             (declare (stream out))
              (call-next-method)
              (when (slot-boundp cc 'time)
                (format out " [timeout ~a sec]" (timeout-time cc))))))
@@ -481,6 +490,7 @@ The argument can be:
    (mesg :type simple-string :reader code-mesg :initarg :mesg :initform "")
    (args :type list :reader code-args :initarg :args :initform nil))
   (:report (lambda (cc out)
+             (declare (stream out))
              (format out "[~s] ~?" (code-proc cc) (code-mesg cc)
                      (code-args cc)))))
 
@@ -1157,61 +1167,81 @@ This is mostly a debugging function, to be called interactively."
 It is bound only by `with-open-html'.")
 (makunbound '*html-output*)
 
-(defmacro with-tag ((tag &rest options &key (close t) (terpri t)
-                         &allow-other-keys)
+(defmacro with-tag ((tag &rest options &key (close t) value
+                         (terpri (and close (not value))) &allow-other-keys)
                     &body forms)
-  (remf options :close) (remf options :terpri)
+  (remf options :close) (remf options :terpri) (remf options :value)
   `(progn
     (when ,terpri (fresh-line *html-output*))
     (format *html-output* "<~a~@{ ~a=~s~}>" ,tag ,@options)
+    (when ,value (princ ,value *html-output*))
     (when ,terpri (terpri *html-output*))
     ,@forms
     (when ,close (format *html-output* "~@[~&~*~]</~a>" ,terpri ,tag))))
 
 (defmacro with-open-html (((&rest open-pars)
-                           (&key (doctype '(html public
-                                            "-//W3C//DTD HTML 3.2//EN"))
+                           (&key (doctype ''(html public
+                                             "-//W3C//DTD HTML 3.2//EN"))
                                  (meta '(:http-equiv "Content-Type"
                                          :content "text/html"))
-                                 base (title "untitled")))
+                                 base comment (title "untitled") (footer t)
+                                 head))
                           &body body)
   "Output HTML to a file."
   `(with-open-file (*html-output* ,@open-pars)
-    (format *html-output* "<!doctype~{ ~s~}>~%" ',doctype)
-    (with-tag (:meta :close nil ,@meta)
-      (terpri *html-output*))
+    (format *html-output* "<!doctype~{ ~s~}>~%" ,doctype)
+    ;; print the comment
+    (format *html-output* "<!--~% File: <~a - " ,(car open-pars))
+    (current-time *html-output*)
+    (format *html-output*
+     " ~a@~a>~% Created by `with-open-html'~% Lisp: ~a ~a~@[~%~a~]~% -->~%"
+     (getenv "USER") (machine-instance) (lisp-implementation-type)
+     (lisp-implementation-version) ,comment)
+    (with-tag (:meta :close nil ,@meta))
+    (terpri *html-output*)
     (when ,base
-      (with-tag (:base :close nil :href ,base)
-        (terpri *html-output*)))
+      (with-tag (:base :close nil :href ,base))
+      (terpri *html-output*))
     (with-tag (:html)
       (terpri *html-output*)
-      (with-tag (:head)
-        (with-tag (:title :terpri nil)
-          (format *html-output* "~a" ,title)))
+      (with-tag (:head ,@head) (with-tag (:title :value ,title)))
       (terpri *html-output*) (terpri *html-output*)
       (with-tag (:body)
-        ,@body))))
+        ,@body
+        (when ,footer
+          (terpri *html-output*) (terpri *html-output*)
+          (with-tag (:p :terpri nil)
+            (with-tag (:hr :close nil))
+            (with-tag (:address :terpri nil)
+              (with-tag (:a :href (format nil "mailto:~a" *user-mail-address*)
+                            :value *user-mail-address*)))
+            (terpri *html-output*) (with-tag (:br :close nil))
+            (with-tag (:strong :value (current-time nil)))))))
+    (terpri *html-output*)))
 
-(defun directory-index (dir file &key (title (format nil "Index of ~a" dir)))
+(defun directory-index (dir file &rest opts
+                        &key (title (format nil "Index of ~a" dir)))
   "Output the index for a directory."
   ;; (directory-index "/etc/*" "/tmp/z.html")
   (with-open-html ((file :direction :output)
-                   (:title title))
+                   (:title title :comment
+                    (format nil " Called: (directory-index ~s ~s~{ ~s~})"
+                            dir file opts)))
     (with-tag (:h1 :terpri nil) (format *html-output* "Index of ~a" dir))
     (terpri *html-output*) (terpri *html-output*)
     (with-tag (:table :border 1)
-      (dolist (fi (sort (directory dir) #'string< :key #'namestring))
+      (dolist (fi (sort (directory dir #+cmu :follow-links #+cmu nil)
+                        #'string< :key #'namestring))
         (with-tag (:tr)
           (with-tag (:th :align "left" :terpri nil)
-            (with-tag (:a :terpri nil :href (namestring fi))
-              (format *html-output* "~a" fi)))
+            (with-tag (:a :href (namestring fi) :value fi)))
           (terpri *html-output*)
           (with-tag (:td :align "right" :terpri nil)
             (format *html-output* "~:d" (ignore-errors (file-size fi))))
           (terpri *html-output*)
-          (with-tag (:td :align "right" :terpri nil)
-            (format *html-output* "~a"
-                    (ignore-errors (dttm->string (file-write-date fi))))))))))
+          (with-tag (:td :align "right" :value
+                         (ignore-errors (dttm->string (file-write-date
+                                                       fi))))))))))
 
 (provide "url")
 ;;; }}} url.lisp ends here

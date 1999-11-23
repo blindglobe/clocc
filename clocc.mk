7510ab9c83e69e0049b6fbc5b9fd4ba938b686f5
sds <sds>
1999-11-23 22:59:27 +0000

    move the standard targets to clocc.mk

--- clocc.mk
+++ clocc.mk
@@ -1,6 +1,15 @@
+# -*- Makefile -*-
 # Common Makefile rules.
 # The variable TOP must already have been set.
 
 RUNLISP := $(TOP)/bin/run-lisp
 FASLEXT := $(shell $(RUNLISP) -faslext)
-FASLFILES = *.fas *.lib  *.axpf *.x86f *.hpf *.sgif *.sparcf  *.fasl  *.o *.data
+FASLFILES = *.fas *.lib *.axpf *.x86f *.hpf *.sgif *.sparcf *.fasl *.o *.data
+
+default: force
+	@echo " * you must specify a target, e.g., 'all'";
+
+clean: force
+	rm -f $(FASLFILES)
+
+force:

5a0ef624c6b544e131919962e172a1591c11df15
sds <sds>
1999-11-23 23:29:51 +0000

    move more targets to the main clocc.mk file

--- clocc.mk
+++ clocc.mk
@@ -3,11 +3,16 @@
 # The variable TOP must already have been set.
 
 RUNLISP := $(TOP)/bin/run-lisp
+LISPFILE := $(TOP)/bin/lisp-file
 FASLEXT := $(shell $(RUNLISP) -faslext)
-FASLFILES = *.fas *.lib *.axpf *.x86f *.hpf *.sgif *.sparcf *.fasl *.o *.data
+FASLFILES = *.fas *.lib *.axpf *.x86f *.hpf *.sgif *.sparcf *.fasl \
+	*.o *.data *.ufsl
 
 default: force
-	@echo " * you must specify a target, e.g., 'all'";
+	@echo " * you must specify a target, e.g., 'all'."
+
+%.$(FASLEXT): $(shell $(LISPFILE) %)
+	$(RUNLISP) -c $<
 
 clean: force
 	rm -f $(FASLFILES)

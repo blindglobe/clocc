369325547fb1275c9ab16b15627eaa405b285424
sds <sds>
1999-11-30 21:05:26 +0000

    Require new variables SOURCES and LISPEXT. (%.$(FASLEXT)): use LISPEXT. (all): use SOURCES. (ChangeLog): new rule.

--- clocc.mk
+++ clocc.mk
@@ -1,6 +1,6 @@
 # -*- Makefile -*-
 # Common Makefile rules.
-# The variable TOP must already have been set.
+# The variables TOP, SOURCES and LISPEXT must already have been set.
 
 RUNLISP := $(TOP)/bin/run-lisp
 LISPFILE := $(TOP)/bin/lisp-file
@@ -9,10 +9,18 @@ FASLFILES = *.fas *.lib *.axpf *.x86f *.hpf *.sgif *.sparcf *.fasl \
 	*.o *.data *.ufsl
 
 default: force
-	@echo " * you must specify a target, e.g., 'all'."
+	@echo " * you must specify a target, e.g., 'all' or 'ChangeLog'."
 
-%.$(FASLEXT): $(shell $(LISPFILE) %)
-	$(RUNLISP) -c $<
+all: $(addsuffix .$(FASLEXT),$(SOURCES))
+
+%.$(FASLEXT): %.$(LISPEXT)
+	$(RUNLISP) $(patsubst %,-i %,$(filter-out $<,$^)) -c $<
+
+ChangeLog: $(addsuffix .$(LISPEXT),$(SOURCES))
+	rcs2log $^ > $@
+
+#.DEFAULT:
+#	@echo "no rule for '$@'; FASLEXT: '$(FASLEXT)'; LISPEXT: '$(LISPEXT)'; LISPFILE: '$(shell $(LISPFILE) $@)'";
 
 clean: force
 	rm -f $(FASLFILES)

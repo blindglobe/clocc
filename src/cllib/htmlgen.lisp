a8dbb75f4326df905bc8b2c22459e633cd5de001
sds <sds>
2000-03-09 19:02:59 +0000

    (html-stream): new Gray stream (with-html-output): new macro, `with-tag' is defined inside it  with `macrolet' (with-tag, with-open-html): removed (directory-index): modified accordingly

--- src/cllib/htmlgen.lisp
+++ src/cllib/htmlgen.lisp
@@ -1,4 +1,4 @@
-;;; File: <htmlgen.lisp - 2000-03-03 Fri 12:14:02 EST sds@ksp.com>
+;;; File: <htmlgen.lisp - 2000-03-08 Wed 21:20:22 Eastern Standard Time sds@ksp.com>
 ;;;
 ;;; HTML generation
 ;;;
@@ -6,107 +6,114 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: htmlgen.lisp,v 1.1 2000/03/03 17:25:19 sds Exp $
+;;; $Id: htmlgen.lisp,v 1.2 2000/03/09 19:02:59 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/htmlgen.lisp,v $
 
 (eval-when (compile load eval)
   (require :base (translate-logical-pathname "clocc:src;cllib;base")))
+;; `dttm->string' - needed only for `directory-index'
+;; (require :date (translate-logical-pathname "cllib:date"))
 
 (in-package :cllib)
 
-(eval-when (load compile eval)
-  (declaim (optimize (speed 3) (space 0) (safety 3) (debug 3))))
+(export '(with-html-output directory-index html-stream))
+
+;;;
+;;; preparation
+;;;
+
+(defcustom *html-chars* list '((#\< . "&lt;") (#\> . "&gt;") (#\& . "&amp;"))
+  "The characters which must be replaced before putting a string into HTML.")
+
+(defclass html-stream (lisp:fundamental-character-output-stream)
+  ((target-stream :initarg :stream :type stream)))
+(defmethod lisp:stream-write-char ((stream html-stream) ch)
+  (with-slots (target-stream) stream
+    (let ((char-cons (assoc ch *html-chars* :test #'char=)))
+      (if char-cons (write-string (cdr char-cons) target-stream)
+          (write-char ch target-stream)))))
+(defmethod lisp:stream-line-column ((stream html-stream)) nil)
+(defmethod lisp:stream-finish-output ((stream html-stream))
+  (with-slots (target-stream) stream (finish-output target-stream)))
+(defmethod lisp:stream-force-output ((stream html-stream))
+  (with-slots (target-stream) stream (force-output target-stream)))
+(defmethod lisp:stream-clear-output ((stream html-stream))
+  (with-slots (target-stream) stream (clear-output target-stream)))
 
 ;;;
 ;;; HTML generation
 ;;;
 
-(defcustom *html-output* stream *standard-output*
-  "The stream where the HTML is printed by `with-tag' and `with-open-html'.
-It is bound only by `with-open-html'.")
-(makunbound '*html-output*)
+(defmacro with-html-output ((var stream
+                             &key (doctype ''(html public
+                                              "-//W3C//DTD HTML 3.2//EN"))
+                                  (meta '(:http-equiv "Content-Type"
+                                          :content "text/html"))
+                                  base comment (title "untitled") (footer t)
+                                  head)
+                            &body body)
+  (with-gensyms ("HTML-" raw mailto)
+    `(let* ((,raw ,stream) (,var (make-instance 'html-stream :stream ,raw))
+            (,mailto (concatenate 'string "mailto:" *user-mail-address*)))
+      (macrolet ((with-tag ((tag &rest options) &body forms)
+                   `(progn (format ,',raw "<~a~@{ ~a=~s~}>" ,tag ,@options)
+                     ,@forms (format ,',raw "</~a>~%" ,tag)))
+                 (with-tagl ((tag &rest options) &body forms)
+                   `(progn (format ,',raw "<~a~@{ ~a=~s~}>" ,tag ,@options)
+                     ,@forms (format ,',raw "</~a>" ,tag))))
+        (unwind-protect
+             (progn
+               (format ,raw "<!doctype~{ ~s~}>~%" ,doctype)
+               ;; print the comment
+               (format ,raw "<!--~% Created on ") (current-time ,raw)
+               (format ,raw "~% by ~a@~a~% using `with-open-html'
+ Lisp: ~a ~a~@[~%~a~]~% -->~2%"
+                       (getenv "USER") (machine-instance)
+                       (lisp-implementation-type) (lisp-implementation-version)
+                       ,comment)
+               (when ,base
+                 (with-tag (:base :href ,base)))
+               (with-tag (:html)
+                 (with-tag (:head ,@head)
+                   (with-tag (:meta ,@meta))
+                   (with-tag (:link :rev 'made :href ,mailto))
+                   (with-tag (:title) (princ ,title ,var)))
+                 (with-tag (:body)
+                   ,@body
+                   (when ,footer
+                     (with-tag (:p)
+                       (with-tag (:hr))
+                       (with-tag (:address)
+                         (with-tag (:a :href ,mailto)
+                           (princ *user-mail-address* ,var)))
+                       (with-tagl (:strong)
+                         (princ (current-time nil) ,var)))))))
+          (when ,raw (close ,raw)))))))
 
-(defmacro with-tag ((tag &rest options &key (close t) value
-                         (terpri (and close (not value))) &allow-other-keys)
-                    &body forms)
-  (remf options :close) (remf options :terpri) (remf options :value)
-  `(progn
-    (when ,terpri (fresh-line *html-output*))
-    (format *html-output* "<~a~@{ ~a=~s~}>" ,tag ,@options)
-    (when ,value (princ ,value *html-output*))
-    (when ,terpri (terpri *html-output*))
-    ,@forms
-    (when ,close (format *html-output* "~@[~&~*~]</~a>" ,terpri ,tag))))
-
-(defmacro with-open-html (((&rest open-pars)
-                           (&key (doctype ''(html public
-                                             "-//W3C//DTD HTML 3.2//EN"))
-                                 (meta '(:http-equiv "Content-Type"
-                                         :content "text/html"))
-                                 base comment (title "untitled") (footer t)
-                                 head))
-                          &body body)
-  "Output HTML to a file."
-  (with-gensyms ("WOH-MAILTO-" mailto)
-    `(let ((,mailto (concatenate 'string "mailto:" *user-mail-address*)))
-      (with-open-file (*html-output* ,@open-pars)
-        (format *html-output* "<!doctype~{ ~s~}>~%" ,doctype)
-        ;; print the comment
-        (format *html-output* "<!--~% File: <~a - " ,(car open-pars))
-        (current-time *html-output*)
-        (format *html-output*
-         " ~a@~a>~% Created by `with-open-html'~% Lisp: ~a ~a~@[~%~a~]~% -->~%"
-         (getenv "USER") (machine-instance) (lisp-implementation-type)
-         (lisp-implementation-version) ,comment)
-        (terpri *html-output*)
-        (when ,base
-          (with-tag (:base :close nil :href ,base))
-          (terpri *html-output*))
-        (with-tag (:html)
-          (terpri *html-output*)
-          (with-tag (:head ,@head)
-            (with-tag (:meta :close nil ,@meta))
-            (terpri *html-output*)
-            (with-tag (:link :close nil :rev 'made :href ,mailto))
-            (terpri *html-output*)
-            (with-tag (:title :value ,title)))
-          (terpri *html-output*) (terpri *html-output*)
-          (with-tag (:body)
-            ,@body
-            (when ,footer
-              (terpri *html-output*) (terpri *html-output*)
-              (with-tag (:p :terpri nil)
-                (with-tag (:hr :close nil))
-                (with-tag (:address :terpri nil)
-                  (with-tag (:a :href ,mailto :value *user-mail-address*)))
-                (terpri *html-output*) (with-tag (:br :close nil))
-                (with-tag (:strong :value (current-time nil)))))))
-        (terpri *html-output*)))))
-
+;;;
 ;;; this is an example on how to use `with-open-html' and `with-tag'.
+;;;
+
 (defun directory-index (dir file &rest opts
                         &key (title (format nil "Index of ~a" dir)))
   "Output the index for a directory."
   ;; (directory-index "/etc/*" "/tmp/z.html")
-  (with-open-html ((file :direction :output)
-                   (:title title :comment
-                    (format nil " Called: (directory-index ~s ~s~{ ~s~})"
-                            dir file opts)))
-    (with-tag (:h1 :terpri nil) (format *html-output* "Index of ~a" dir))
-    (terpri *html-output*) (terpri *html-output*)
-    (with-tag (:table :border 1)
+  (with-html-output (out (open file :direction :output)
+                     :title title :comment
+                     (format nil " Called: (directory-index ~s ~s~{ ~s~})"
+                             dir file opts))
+    (with-tag (:h1) (format out "Index of ~a" dir))
+    (with-tag (:table :border "1")
       (dolist (fi (sort (directory dir #+cmu :follow-links #+cmu nil)
                         #'string< :key #'namestring))
         (with-tag (:tr)
-          (with-tag (:th :align "left" :terpri nil)
-            (with-tag (:a :href (namestring fi) :value fi)))
-          (terpri *html-output*)
-          (with-tag (:td :align "right" :terpri nil)
-            (format *html-output* "~:d" (ignore-errors (file-size fi))))
-          (terpri *html-output*)
-          (with-tag (:td :align "right" :value
-                         (ignore-errors (dttm->string (file-write-date
-                                                       fi))))))))))
+          (with-tag (:th :align "left")
+            (with-tag (:a :href (namestring fi)) (princ fi out)))
+          (with-tag (:td :align "right")
+            (format out "~:d" (ignore-errors (file-size fi))))
+          (with-tag (:td :align "right")
+            (princ (ignore-errors (dttm->string (file-write-date fi)))
+                   out)))))))
 
 
 (provide :htmlgen)

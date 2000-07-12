57540a5cfb388300cde965e548c318fce2786ffd
rtoy <rtoy>
2000-07-12 22:00:25 +0000

    Make this program actually compile and run.  Fix the syntax errors. Change some of the code so that it doesn't play games with function and variable types and stuff.  (These definitely confuse f2cl and the resulting Lisp code.)  With these changes, the result compiles with Fortran and f2cl, and they produce equivalent outputs.

--- src/f2cl/val/stmtfntest.for
+++ src/f2cl/val/stmtfntest.for
@@ -7,8 +7,8 @@ C a function sub program
       END
 C
 C a typed FUNCTION
-      DOUBLE FUNCTION jfun(arg1)
-      DOUBLE arg1
+      DOUBLE PRECISION FUNCTION jfun(arg1)
+      DOUBLE PRECISION arg1
       jfun = SQRT(arg1)
       END
 C
@@ -16,16 +16,19 @@ C subroutine with a stmt function
       SUBROUTINE subr1(a,b)
       DIMENSION arrayA(3,4,3)
       stmtfn(x, y, z) = max(x, max(y, z))
-      a = stmtfn(b,a,a)
-      arrayA(1,1,1) = a
-      write(*,9000),a
-9000  format("Value is "I3/)
+      c = stmtfn(b,a,a)
+      arrayA(1,1,1) = c
+      write(*,9000),c
+9000  format("Value is ",D8.2/)
       RETURN
       END
 
 c subroutine with no args and which calls the functions
       SUBROUTINE subr2
-      IMPLICIT DOUBLE (i,l-n)
+      IMPLICIT DOUBLE PRECISION (l)
+      double precision i
+      double precision jfun
+      integer j, m
       write(*, 9030)
       i=1.
       j=2
@@ -34,14 +37,14 @@ c subroutine with no args and which calls the functions
       write(*,9010), ifun(j,m)
       write(*,9020), jfun(i+l)
       STOP
-9010  FORMAT("IFUN RETURNS "I3) 
-9020  FORMAT("JFUN RETURNS "D3.2)
+9010  FORMAT("IFUN RETURNS ",I3) 
+9020  FORMAT("JFUN RETURNS ",D8.2)
 9030  FORMAT("inside subroutine subr2"/)
       END
 C
 C main
       PROGRAM TEST
-      CALL subr1(3, 4)
+      CALL subr1(3.0, 4.0)
       CALL subr2
       STOP
       END

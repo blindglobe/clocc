f52337261a8109db1ec2a6c24edbf7b16a31b2f4
rtoy <rtoy>
2000-07-28 16:55:40 +0000

    Add exponent option in format statements so we can print out the large exponents.

--- src/f2cl/packages/toms/715/algtst.f
+++ src/f2cl/packages/toms/715/algtst.f
@@ -259,21 +259,21 @@ C-----------------------------------------------------------------
      2    10X,'was smaller',I6,' times.'//)
  1020 FORMAT(' There are',I4,' base',I4,
      1    ' significant digits in a floating-point number'//)
- 1021 FORMAT(' The maximum relative error of',E15.4,' = ',I4,' **',
-     1    F7.2/4X,'occurred for X =',E13.6)
+ 1021 FORMAT(' The maximum relative error of',E15.4E3,' = ',I4,' **',
+     1    F7.2/4X,'occurred for X =',E13.6E3)
  1022 FORMAT(' The estimated loss of base',I4,
      1    ' significant digits is',F7.2//)
- 1023 FORMAT(' The root mean square relative error was',E15.4,
+ 1023 FORMAT(' The root mean square relative error was',E15.4E3,
      1    ' = ',I4,' **',F7.2)
  1025 FORMAT('1Special Tests'//)
  1040 FORMAT(//' Test of special arguments'//)
- 1041 FORMAT(' LGAMA (',E13.6,') = ',E13.6//)
+ 1041 FORMAT(' LGAMA (',E13.6E3,') = ',E13.6E3//)
  1050 FORMAT('1Test of Error Returns'///)
- 1052 FORMAT(' LGAMA will be called with the argument',E13.6,/
+ 1052 FORMAT(' LGAMA will be called with the argument',E13.6E3,/
      1    ' This should trigger an error message'//)
- 1053 FORMAT(' LGAMA will be called with the argument',E13.6,/
+ 1053 FORMAT(' LGAMA will be called with the argument',E13.6E3,/
      1    ' This should not trigger an error message'//)
- 1061 FORMAT(' LGAMA returned the value',E13.6///)
+ 1061 FORMAT(' LGAMA returned the value',E13.6E3///)
  1100 FORMAT(' This concludes the tests')
 C---------- Last line of ALGAMA/DLGAMA test program ----------
       END

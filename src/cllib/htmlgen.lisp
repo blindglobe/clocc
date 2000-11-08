426fb5b4dffdda1c80bf074b4d7dc9d34fd2758f
sds <sds>
2000-11-08 22:52:22 +0000

    (with-http-output): typos

--- src/cllib/htmlgen.lisp
+++ src/cllib/htmlgen.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: htmlgen.lisp,v 1.7 2000/11/08 22:39:59 sds Exp $
+;;; $Id: htmlgen.lisp,v 1.8 2000/11/08 22:52:22 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/htmlgen.lisp,v $
 
 (eval-when (compile load eval)
@@ -94,8 +94,8 @@ Both print a tag but the second one does not do a `terpri' afterwards."
                              (princ *user-mail-address* ,var)))
                          (with-tagl (:strong) (current-time ,var))))))))))))
 
-(defmacro with-http-output ((var socket &key keep-alive
-                             &rest opts &allow-other-keys)
+(defmacro with-http-output ((var socket &rest opts &key keep-alive
+                             &allow-other-keys)
                             &body body)
   "Write some HTML to an http client on SOCKET.
 Supplies some HTTP/1.0 headers and calls `with-html-output'.
@@ -107,11 +107,10 @@ Returns the lines flushed from the socket."
             (,string (with-output-to-string (,stream)
                        (with-html-output (,var ,stream ,@opts) ,@body))))
       (format ,sock "HTTP/1.0 200 OK~%Content-type: text/html
-Content-length: %d~%Connection: ~:[Close~;Keep-Alive]~2%"
+Content-length: %d~%Connection: ~:[Close~;Keep-Alive~]~2%"
        (length ,string) ,keep-alive)
       (write-string ,string ,sock)
-      (unless keep-alive
-        (close ,sock))
+      (unless ,keep-alive (close ,sock))
       ,flush)))
 
 ;;;

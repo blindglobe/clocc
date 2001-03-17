1a3f6d6ed9888b10196fe8a9a06280de9c590c47
sds <sds>
2001-03-17 19:56:38 +0000

    (socket-accept): ACL5 cannot change socket format

--- src/port/net.lisp
+++ src/port/net.lisp
@@ -8,7 +8,7 @@
 ;;; See <URL:http://www.gnu.org/copyleft/lesser.html>
 ;;; for details and the precise copyright document.
 ;;;
-;;; $Id: net.lisp,v 1.29 2001/01/23 15:54:17 sds Exp $
+;;; $Id: net.lisp,v 1.30 2001/03/17 19:56:38 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/port/net.lisp,v $
 
 (eval-when (compile load eval)
@@ -206,20 +206,27 @@ Returns a socket stream or NIL."
   (declare (type socket-server serv)
            #+(and allegro (version>= 6))
            (ignore bin))
-  #+allegro (let ((sock (if wait
-                            (if (plusp wait)
-                                (mp:with-timeout (wait)
-                                  (socket:accept-connection serv :wait t))
-                                (socket:accept-connection serv :wait nil))
-                            (socket:accept-connection serv :wait t))))
+  #+allegro (let* ((fmt (if bin :binary :text))
+                   #+allegro-v5.0
+                   (excl:*default-external-format* fmt)
+                   (sock (if wait
+                             (if (plusp wait)
+                                 (mp:with-timeout (wait)
+                                   (socket:accept-connection serv :wait t))
+                                 (socket:accept-connection serv :wait nil))
+                             (socket:accept-connection serv :wait t))))
               (when sock
                 ;; From: John Foderaro <jkf@franz.com>
                 ;; Date: Sun, 12 Nov 2000 16:58:28 -0800
                 ;; in ACL6 and later, all sockets are bivalent (both
                 ;; text and binary) and thus there's no need to convert
                 ;; between the element types.
-                #+(and allegro (not (version>= 6)))
-                (socket:set-socket-format sock (if bin :binary :text))
+                #+allegro-v5.0
+                (unless (eq (socket:socket-format sock) fmt)
+                  (warn "~s: ACL5 cannot modify socket format"
+                        'socket-accept))
+                #+allegro-v4.3
+                (socket:set-socket-format sock fmt)
                 sock))
   #+clisp (multiple-value-bind (sec usec) (floor (or wait 0))
             (when (lisp:socket-wait serv (and wait sec) (round usec 1d-6))

443d6204186f63338b32b3bdfa60b11da0edb38b
sds <sds>
2001-04-20 19:27:52 +0000

    (test-date): use dttm->string :short

--- src/cllib/tests.lisp
+++ src/cllib/tests.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: tests.lisp,v 2.6 2000/09/29 17:37:58 sds Exp $
+;;; $Id: tests.lisp,v 2.7 2001/04/20 19:27:52 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/tests.lisp,v $
 
 (eval-when (load compile eval)
@@ -80,7 +80,8 @@
               (incf num-err)
               (warn " ### FAILED: ~6:d --> ~a --> ~6:d~2%" n0 dd n1))))
     (flet ((ts (nn st)
-             (mesg :test out "~30s --> ~d --> ~a~%" st nn (dttm->string nn))
+             (mesg :test out "~30s --> ~d --> ~a~%"
+                   st nn (dttm->string nn :short))
              (unless (= nn (string->dttm st))
                (incf num-err)
                (warn " ### FAILED: ~s --> ~d, not ~d~2%"

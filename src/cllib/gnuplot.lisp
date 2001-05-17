564fbb065c98d18a7751f3539279d7da223ba2bf
sds <sds>
2001-05-17 17:07:51 +0000

    rewrote the backend in the object-oriented way

--- src/cllib/gnuplot.lisp
+++ src/cllib/gnuplot.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: gnuplot.lisp,v 2.14 2001/05/17 17:07:47 sds Exp $
+;;; $Id: gnuplot.lisp,v 3.0 2001/05/17 17:07:51 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/gnuplot.lisp,v $
 
 ;;; the main entry point is WITH-PLOT-STREAM

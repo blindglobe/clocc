dbab6a2b222dba571f533c1b40a2ca586d7e6b52
pvaneynd <pvaneynd>
2001-06-03 22:30:41 +0000

    first version Pre-release mind you...

---
+++ src/port/common-lisp-controller/README.design
@@ -0,0 +1,139 @@
+		Design of the Common Lisp controller
+
+	Introduction
+
+At present there is no clear cut way for a Common Lisp implementation
+to find a installed Common Lisp library. Ad-hoc solutions are just not
+good enough:
+
+	- user's want to be able to see the source code of the library
+	- libraries sometimes need to be recompiled when an
+	  incompatible version of the implementation has been used to
+	  compile the current fasl files.
+	- implementations and libraries need a common protocol to find
+	  each other.
+
+In this document I propose a solution to these problems, these idea's
+will form the basis of a Debian package Common-Lisp-Controller that
+will implement the needed infrastructure. Each Common Lisp
+Implementation and Library package for Debian should then follow a few
+simple guidelines so 
+(require 'nirvana)
+should 'just work'.
+
+	Basic Roadmap
+
+We use logical pathnames to point implementations to the
+libraries. The setting up of the logical pathnames is the
+responsibility of the implementation.
+
+The libraries and system files are installed in pre-defined locations
+so they can be found automagicly. 
+
+The main idea is the following:
+
+Put the source trees under:
+/usr/share/common-lisp/source/<package-name>
+
+This can, and in  most cases will be a symbolic link to
+a directory in
+/usr/share/common-lisp/repositories/...
+
+Here there is a place to put for example sourceforge-hosted
+libraries. These can then be automaticly updated with cvs
+commands. Note too that <package-name> has to be a portable pathname,
+so can only contain A-Z0-9- (a-z also seems to work most of the
+time). Placing the true files in the repository directory and making
+only a confirming symbolic link can help in getting a valid logical
+pathname (for example we link /usr/share/common-lisp/source/defsystem
+to
+/usr/share/common-lisp/repositories/sourceforge/clocc/clocc/src/defsystem-3.x).
+
+The system definitions in
+/usr/share/common-lisp/systems/<package-name>.system
+In those system files you should reference your sourcecode via the
+"cl-library:;<package-name>;foo.lisp" logical pathname.
+FIXME: for sbcl only "cl-library:<package-name>;foo.lisp" works!
+
+The MK:DEFSYSTEM will then compile that file to 
+/usr/lib/common-lisp/<name>/<package-name>/foo.<fasl of that lisp> 
+As this is done with the logical pathname stuff the implementation
+should worry about the <name> and <fasl of that lisp> part.
+
+	Common-Lisp-Controller
+	
+This is the basic setup. The remainder is more part of
+Common-Lisp-Controller and the general care and maintenance of a
+system. After installation a Common Lisp Implementation should put a
+script in 
+/usr/lib/common-lisp/bin/<name>.sh
+with name the name of the implementation. Notice the /usr/lib. This is
+because we want to be able to share /usr/share between different
+machines. As a Common Lisp Implementation is pretty bound to a certain
+machine I think that file should go there.
+
+Every common lisp code package should copy the files in the source and
+systems directories. Then register with:
+
+register-common-lisp-source <package-name>
+
+Every common lisp implementation should install help scripts and
+binaries in the bin directory and then register with: 
+
+register-common-lisp-implementation <name>
+
+The name should be the name of the implementation, eg. "cmucl-safe".
+
+The script should be a shell script or binary accepting the following
+commands:
+
+	<name>.sh rebuild <package-name>+
+The should load and recompile the given packages. It should
+load and use mk-defsystem automagicly. It should also if possible
+create the libraries for that implementation.
+<run as nobody>
+
+	<name>.sh remove <package-name>*
+This should remove all fasl, log and other files belonging
+to the package
+<run as nobody>
+	
+	<name>.sh install-defsystem
+This should compile and install defsystem in the default image.
+<run as root>
+
+	<name>.sh remove-defsystem
+This should remove the compiled defsystem and support in the default
+images.
+<run as root>
+
+	<name>.sh make-user-image <file> 
+This should load and evalute the <file> and then dump a lisp image into
+the users home directory.
+<run as the user>
+
+Notice that there are subtle security problems here: we don't want
+something as powerful as a common lisp compiler running as root, but
+we need to have root as the owner of the fasl's for obvious
+reasons. The solution is to re-chown the /usr/lib/common-lisp hierarchy
+to the user "nobody", compile the stuff as user "nobody" and then
+re-chown the hierarchy back to root. In this was we run the compiler
+as a user who can do little harm, and at the same time the resulting
+files will be safe. Notice that we should change the umask to 077 to
+avoid generating writable files. 
+
+All of these commands are run as 'nobody', except install-defsystem
+and remove-defsystem because they modify files outside of the
+control of common-lisp-controller.
+
+Please note that the lisp implementations should load as cleanly as
+possible and ignore any configuration and user-specific files and 
+configuration options.
+
+To remove a common lisp code package:
+unregister-common-lisp-source <package-name>
+this commands also removes all compiled files and library files.
+
+To remove a common lisp compiler:
+unregister-common-lisp-implementation <name> <sub-name>
+All relevant fasl's will be removed.
---
+++ src/port/common-lisp-controller/common-lisp-controller.lisp
@@ -0,0 +1,190 @@
+;;; -*- Mode: Lisp; Package: COMMON-LISP-CONTROLLER -*-
+
+(in-package :cl-user)
+
+(defpackage "COMMON-LISP-CONTROLLER" 
+    (:use "COMMON-LISP")
+  (:export "INIT-COMMON-LISP-CONTROLLER"
+  	   "ADD-TRANSLATION")
+  (:nicknames "C-L-C"))
+
+(in-package :common-lisp-controller)
+
+;; Some general utilities to make the
+;; descriptions shorter
+
+(defun add-translation (for new-root new-part)                           
+  "Adds a translation to the logical pathname named by FOR (:cl-library or :cl-systems)
+NEW-ROOT is the new root for this translation, NEW-PART is the part below the
+root that should be added.
+For example
+ (add-translation :cl-library \"/home/pvaneynd/junk-pile/\"
+                  (make-pathname :directory '(:RELATIVE \"HEMLOCK\" :wild-inferiors)
+                                 :type \"FASL\"
+                                 :case :common))
+should add a translation that
+cl-library:;hemlock;**;*.fasl.* -> /home/pvaneynd/junk-pile/hemlock/**/*.fasl
+
+This function returns nothing."
+  (let ((lp-host (ecase for
+                   (:cl-library
+                    "cl-library")
+                   (:cl-systems
+                    "cl-systems")))
+        ;; force to pathnames
+        (new-root (pathname new-root))
+        (new-part (pathname new-part)))
+    
+    (assert (eq (first (pathname-directory new-part))
+                :relative)
+            (new-part)
+            "The NEW-PART parameter ~S is not relative to something, it has to be"
+            new-part)
+    
+    (let ((new-source
+           ;; construct based on new-part but in the right logical pathname
+           (make-pathname :defaults new-part
+                          :host lp-host))
+          ;; construct the destination, based on all this
+          (new-dest
+           (make-pathname :defaults new-part
+                          :case :common
+                          ;; but under new-root
+                          :directory (append (pathname-directory new-root
+                                                                 :case :common)
+                                             ;; skip the relative
+                                             (rest (pathname-directory new-part
+                                                                       :case :common))))))
+      (push (list new-source
+                  new-dest)
+            (logical-pathname-translations lp-host)))
+    ;; also support the old way
+    (let ((new-source
+           ;; construct based on new-part but in the right logical pathname
+           (make-pathname :defaults new-part
+                          :directory (cons :absolute
+                                           (rest
+                                            (pathname-directory new-part
+                                                                :case :common)))
+                          :case :common
+                          :host lp-host))
+          ;; construct the destination, based on all this
+          (new-dest
+           (make-pathname :defaults new-part
+                          :case :common
+                          ;; but under new-root
+                          :directory (append (pathname-directory new-root
+                                                                 :case :common)
+                                             ;; skip the relative
+                                             (rest (pathname-directory new-part
+                                                                       :case :common))))))
+      (push (list new-source
+                  new-dest)
+            (logical-pathname-translations lp-host)))
+    (values)))
+      
+                           
+(defun init-common-lisp-controller (fasl-root
+                                    &key (source-root "/usr/share/common-lisp/"))
+  "configures FASL-ROOT as the base of the fasl tree and optionally
+SOURCE-ROOT as the root of the source tree.
+NOTE: NUKES the cl-library and cl-systems LOGICAL PATHNAMES
+
+Returns nothing"
+  ;; force both parameters to directories...
+  (let* ((fasl-root (make-pathname :name nil :type nil :version nil
+                                   :case :common
+                                   :defaults (pathname fasl-root)))
+         (s-root (pathname source-root))
+         (source-root (make-pathname
+                       :defaults s-root
+                       :case :common
+                       :directory (append (pathname-directory s-root
+                                                              :case :common)
+                                          '("SOURCE"))))
+         (system-root (make-pathname
+                       :defaults s-root
+                       :case :common
+                       :directory (append (pathname-directory s-root
+                                                              :case :common)
+                                          '("SYSTEMS")))))
+    (setf (logical-pathname-translations "cl-library")
+          nil)
+    (setf (logical-pathname-translations "cl-systems")
+          nil)
+        ;;; by default everything is in the fasl tree...
+    (setf (logical-pathname-translations "cl-library")
+          (list
+           (list (make-pathname :directory '(:relative :wild-inferiors)
+                                :host (pathname-host (logical-pathname "CL-LIBRARY:")
+                                                     :case :common)
+                                :case :common)
+                 ;; ;**;*.*.*
+                 ;; to                 
+                 (make-pathname :directory (append (pathname-directory fasl-root
+                                                                       :case :common)
+                                                   (list :wild-inferiors))
+                                :case :common
+                                :defaults fasl-root))
+           (list (make-pathname :directory '(:absolute :wild-inferiors)
+                                :host (pathname-host (logical-pathname "CL-LIBRARY:")
+                                                     :case :common)
+                                :case :common)
+                 ;; ;**;*.*.*
+                 ;; to                 
+                 (make-pathname :directory (append (pathname-directory fasl-root
+                                                                       :case :common)
+                                                   (list :wild-inferiors))
+                                :case :common
+                                :defaults fasl-root))))
+    ;;; cmucl-hemlock stuff is in the source tree...
+    (add-translation :cl-library
+                     fasl-root
+                     (make-pathname :directory '(:relative "CMUCL-HEMLOCK" :wild-inferiors)
+                                    :type "TEXT"
+                                    :case :common))    
+    ;;; add common source extentions:
+    (loop for extention in '("CL" "LISP"
+                             "L" "LSP"
+			     "C" "H")
+          do
+          (add-translation :cl-library
+                           source-root
+                           (make-pathname :directory '(:relative :wild-inferiors)
+                                          :type extention
+                                          :case :common)))
+    ;; now cl-systems:
+    ;; by default everything is in the fasl tree...
+    (setf (logical-pathname-translations "cl-systems")
+          (list
+           (list (make-pathname :directory '(:relative :wild-inferiors)
+                                :host (pathname-host (logical-pathname "CL-SYSTEMS:")
+                                           :case :common)
+                                :type "SYSTEM"
+                                :case :common)
+                 ;; ;**;*.*.*
+                 ;; to                 
+                 (make-pathname :directory (append (pathname-directory system-root
+                                           :case :common)
+                                                   (list :wild-inferiors))
+                                :type "SYSTEM"
+                                :case :common
+                                :defaults system-root))
+           (list (make-pathname :directory '(:absolute :wild-inferiors)
+                                :host (pathname-host (logical-pathname "CL-SYSTEMS:")
+                                           :case :common)
+                                :type "SYSTEM"
+                                :case :common)
+                 ;; ;**;*.*.*
+                 ;; to                 
+                 (make-pathname :directory (append (pathname-directory system-root
+                                           :case :common)
+                                                   (list :wild-inferiors))
+                                :type "SYSTEM"
+                                :case :common
+                                :defaults system-root))))
+    (values)))
+
+(eval-when (:load-toplevel :execute :compile-toplevel)
+  (pushnew :common-lisp-controller *features*))
+

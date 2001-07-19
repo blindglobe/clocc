6632c2d7b6f63b5fab8348e96bf3c274c00b6673
sds <sds>
2001-07-19 17:38:40 +0000

    added ldiff/tailp test from CLHS

--- src/tools/ansi-test/alltest.lisp
+++ src/tools/ansi-test/alltest.lisp
@@ -2808,6 +2808,26 @@
 	'e)
  (a b c d))
 
+(my-assert
+ (let ((lists '#((a b c) (a b c . d)))
+       (ld-res #(#(nil (a b) (a b c) (a b c) (a b c) (a b c) (a b c))
+                 #(nil (a b) (a b c . d) (a b c . d) (a b c . d) (a b c)
+                   (a b c . d))))
+       (tp-res #(#(t t nil nil t nil nil) #(t t nil nil nil t nil))))
+   (dotimes (i (length lists))
+     (let* ((list (aref lists i)) (l-r (aref ld-res i)) (t-r (aref tp-res i))
+            (objects (vector list (cddr list) (copy-list (cddr list))
+                             '(f g h) '() 'd 'x)))
+       (dotimes (j (length objects))
+         (let ((object (aref objects j)))
+           (unless (equal (tailp object list) (aref t-r j))
+             (error "(tailp ~s ~s): ~s; should be: ~s"
+                    object list (tailp object list) (aref t-r j)))
+           (unless (equal (ldiff list object) (aref l-r j))
+             (error "(ldiff ~s ~s): ~s; should be: ~s"
+                    list object (ldiff list object) (aref l-r j))))))))
+ nil)
+
 ;; rplaca, rplacd
 
 (my-assert

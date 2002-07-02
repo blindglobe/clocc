df3fe92be125161463bf8b46b37500b66bbbb0b7
rtoy <rtoy>
2002-07-02 22:12:37 +0000

    Simple script to run the tests.

---
+++ src/f2cl/packages/minpack/run-minpack-tests.lisp
@@ -0,0 +1,9 @@
+(in-package "MINPACK")
+
+(defun run-minpack-tests ()
+  (dolist (f '(tlmdif tlmder))
+    (with-open-file (input "clocc:src;f2cl;packages;minpack;lmdif-input.dat"
+			   :direction :input)
+      (let ((*standard-input* input))
+	(funcall f)))))
+  

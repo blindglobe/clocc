e5d71fa40443828caeb1fd62f62d21aef7bbcb8e
marcoxa <marcoxa>
2002-07-18 20:08:10 +0000

    Changed the hierarchy structure to reflect changes in the predefined components.

--- src/defsystem-4/src/predefined-specialized-components.lisp
+++ src/defsystem-4/src/predefined-specialized-components.lisp
@@ -13,27 +13,37 @@
   ()
   )
 
-(defclass c-header-file (c-file c-language-mixin)
+(defclass c-header-file (c-file)
   ()
   (:default-initargs :source-extension "h"))
 
-(defclass c-executable (c-file)
+(defclass c-executable (c-file executable-file)
+  ()
+  (:default-initargs :link t))
+
+(defclass c-object (c-file object-file)
   ()
   (:default-initargs :link t))
 
 
-(defclass statically-linked-library (library c-language-mixin)
+;;; Notes:
+;;;
+;;; 2002-05-22 Marco Antoniotti
+;;; Originally these components were defined to have :language :c.
+;;; This is not necessarily the case.
+;;;
+;;; This realization is a good thing, because it settles the dilemma of
+;;; the inherited language mixin vs. the language slot.  The language
+;;; slot is what I want.
+
+(defclass statically-linked-library (library)
   ()
-  (:default-initargs
-    :language :c
-    :binary-extension (static-library-extension))
+  (:default-initargs :binary-extension (static-library-extension))
   )
 
-(defclass dynamically-linked-library (library c-language-mixin)
+(defclass dynamically-linked-library (library)
   ()
-  (:default-initargs
-    :language :c
-    :binary-extension (shared-library-extension))
+  (:default-initargs :binary-extension (shared-library-extension))
   )
 
 ;;; end of file -- predefined-specialized-components.lisp --

1d92f7eb8eb2ff7d99882ca15e258d02c7f6f1a8
marcoxa <marcoxa>
2002-11-14 15:35:13 +0000

    Checkpoint version.

--- src/defsystem-4/src/impl-dependent/lispworks.lisp
+++ src/defsystem-4/src/impl-dependent/lispworks.lisp
@@ -34,6 +34,23 @@
 (defun run-program (program &rest arguments)
   (system:call-system-showing-output (format nil "~A~@[~{ ~A~}~]"
 					      program arguments)))
-  
+
+
+(defmethod load-object-file ((loadable-object-pathname pathname)
+			     &key
+			     (print *load-print*)
+			     (verbose *load-verbose*)
+			     (libraries '("c"))
+			     )
+  (declare (ignore print libraries))
+  (when verbose
+    (format *trace-output* ";;; MK4: Loading Foreign File ~A."
+	    loadable-c-pathname))
+  #+(and :unix (not :linux))
+  (link-load:read-foreign-module loadable-object-pathname)
+
+  #+(and :unix (not :linux))
+  (fli:register-module loadable-object-pathname)
+  )
 
 ;;; end of file -- lispworks.lisp --

a11e9fe1eadf865b874e424cf2616a3963ca504a
sds <sds>
2002-11-30 22:45:51 +0000

    (with-http-output): use REMOVE-PLIST instead of REMF

--- src/cllib/htmlgen.lisp
+++ src/cllib/htmlgen.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: htmlgen.lisp,v 1.16 2002/07/22 14:15:19 sds Exp $
+;;; $Id: htmlgen.lisp,v 1.17 2002/11/30 22:45:51 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/htmlgen.lisp,v $
 
 (eval-when (compile load eval)
@@ -110,12 +110,11 @@ Both print a tag but the second one does not do a `terpri' afterwards."
   "Write some HTML to an http client on socket stream RAW.
 Supplies some HTTP/1.0 headers and calls `with-html-output'."
   (with-gensyms ("HTTP-" string stream sock header line dbg alive)
-    (remf opts :keep-alive) (remf opts :debug)
-    (remf opts :return-code) (remf opts :return-name)
     `(let* ((,sock ,raw)
             (,dbg ,debug) (,alive ,keep-alive)
             (,string (with-output-to-string (,stream)
-                       (with-html-output (,var ,stream ,@opts) ,@body)))
+                       (with-html-output (,var ,stream ,@(remove-plist opts :keep-alive :debug :return-code :return-name))
+                         ,@body)))
             (,header (list (format nil "HTTP/1.0 ~d ~a"
                                    ,return-code ,return-name)
                            "Content-type: text/html"

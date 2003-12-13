e21aa6f0a0a597148ba8f1c46c95d0e9d8d66ae1
airfoyle <airfoyle>
2003-12-13 02:43:01 +0000

    Fixed bug in use of lprec-find-version-modtimes

--- src/ytools/ytload/raw-ytfm-load.lisp
+++ src/ytools/ytload/raw-ytfm-load.lisp
@@ -22,3 +22,8 @@
    (load (merge-pathnames
 	    (make-pathname :name fname :type lisp-object-extn*)
 	    ytools-bin-dir-pathname*)))
+
+(defun yt-recompile ()
+   (dolist (fname ytools-core-files*)
+      (yt-comp fname)
+      (yt-bload fname)))

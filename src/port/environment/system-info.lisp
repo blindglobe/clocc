78d42c4c89f8e84245f974e17502c7a8a3f0d9a0
marcoxa <marcoxa>
2004-01-08 22:09:59 +0000

    Copyright updated.

--- src/port/environment/system-info.lisp
+++ src/port/environment/system-info.lisp
@@ -5,7 +5,7 @@
 ;;; Added some new interface functions which may turn out to be
 ;;; useful.
 ;;;
-;;; Copyright (c) 2000, 2001 Marco Antoniotti, all rights reserved.
+;;; Copyright (c) 2000-2004 Marco Antoniotti, all rights reserved.
 ;;; This software is released under the terms of the GNU Lesser General
 ;;; Public License (LGPL, see file COPYRIGHT for details).
 

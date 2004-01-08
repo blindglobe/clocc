205b109a859a1c397f4b15399f0bc59ec0f4b7f3
marcoxa <marcoxa>
2004-01-08 22:03:17 +0000

    Minor changes to the file (copyright fixes etc etc)

--- src/port/environment/impl-dependent/cmucl.lisp
+++ src/port/environment/impl-dependent/cmucl.lisp
@@ -1,9 +1,9 @@
-;;; -*- Mode: CLtL -*-
+;;; -*- Mode: Lisp -*-
 
 ;;; cmucl.lisp --
 ;;; CMUCL implementation dependencies.
 
-;;; Copyright (c) 2000, 2001 Marco Antoniotti, all rights reserved.
+;;; Copyright (c) 2000-2004 Marco Antoniotti, all rights reserved.
 ;;; This software is released under the terms of the GNU Lesser General
 ;;; Public License (LGPL, see file COPYING for details).
 

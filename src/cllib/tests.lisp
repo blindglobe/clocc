6ec631d0b88df283e0c6006774e20ced0feccb10
sds <sds>
2004-02-26 19:42:23 +0000

    (test-xml): cl-ent now has 1177 entities

--- src/cllib/tests.lisp
+++ src/cllib/tests.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: tests.lisp,v 2.20 2003/07/11 20:49:55 sds Exp $
+;;; $Id: tests.lisp,v 2.21 2004/02/26 19:42:23 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/tests.lisp,v $
 
 (eval-when (load compile eval)
@@ -193,7 +193,7 @@
                  (incf num-err)))))
       (mesg :test out " ** ~s...~%" 'test-xml)
       (ts *xml-ent-file* 283)
-      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1164))
+      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1177))
     (mesg :test out " ** ~s: ~:d error~:p~2%" 'test-xml num-err)
     num-err))
 

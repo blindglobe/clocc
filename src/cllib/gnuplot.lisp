94815bef8b44d62f12219b513b9decbfe972bb56
sds <sds>
2004-07-30 19:37:03 +0000

    (*gnuplot-printer*): reset to a B/W printed (ps-terminal): do not set TERMINAL-OPTIONS (make-plot-stream/:plot): simplified

--- src/cllib/gnuplot.lisp
+++ src/cllib/gnuplot.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: gnuplot.lisp,v 3.18 2004/07/30 16:36:43 sds Exp $
+;;; $Id: gnuplot.lisp,v 3.19 2004/07/30 19:37:03 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/gnuplot.lisp,v $
 
 ;;; the main entry point is WITH-PLOT-STREAM
@@ -50,7 +50,7 @@ This must be either a full path or a name of an executable in your PATH.")
 (eval-when (compile load eval)  ; CMUCL
 (defcustom *gnuplot-printer* simple-string
   (format nil
-          #+(or win32 mswindows) "//SEATTLE/4th Floor N Color"
+          #+(or win32 mswindows) "//SEATTLE/4th Floor NE"
           #+unix "|lpr -h~@[ -P~a~]"
           (getenv "SDSPRT"))
   "*The printer to print the plots."))
@@ -143,8 +143,7 @@ according to the given backend")
   "The `plot-term' object sending the plot to the printer.")
 
 (defun ps-terminal (file)
-  (make-plot-term :target file :terminal "postscript"
-                  :terminal-options "landscape 'Helvetica' 9"))
+  (make-plot-term :target file :terminal "postscript"))
 (defgeneric directive-term (directive)
   (:documentation "Return the PLOT-TERM object appropriate for this directive")
   (:method ((directive t))
@@ -310,10 +309,9 @@ according to the given backend")
   (:method ((directive pathname)) (open directive :direction :output))
   (:method ((directive (eql :print))) (make-plot-stream :plot))
   (:method ((directive (eql :plot)))
-    (setq *gnuplot-stream*
-          (or (if (and *gnuplot-stream* (open-stream-p *gnuplot-stream*))
-                  *gnuplot-stream*)
-              (pipe-output *gnuplot-path*)))))
+    (unless (and *gnuplot-stream* (open-stream-p *gnuplot-stream*))
+      (setq *gnuplot-stream* (pipe-output *gnuplot-path*)))
+    *gnuplot-stream*))
 
 (defun internal-with-plot-stream (body-function &rest opts
                                   &key (plot *gnuplot-default-directive*)

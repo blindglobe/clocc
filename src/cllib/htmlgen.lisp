420303492883ef09752cc63d9053ef8bed88845b
sds <sds>
2004-11-09 22:00:30 +0000

    (crlf): use WRITE-STRING instead of two WRITE-CHARs

--- src/cllib/htmlgen.lisp
+++ src/cllib/htmlgen.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: htmlgen.lisp,v 1.17 2002/11/30 22:45:51 sds Exp $
+;;; $Id: htmlgen.lisp,v 1.18 2004/11/09 22:00:30 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/htmlgen.lisp,v $
 
 (eval-when (compile load eval)
@@ -100,8 +100,9 @@ Both print a tag but the second one does not do a `terpri' afterwards."
 
 (defun crlf (sock)
   "Write CR/LF into the socket SOCK."
-  (write-char (code-char 13) sock)
-  (write-char (code-char 10) sock))
+  (write-string #.(make-array 2 :element-type 'character :initial-contents
+                              '(#\Return #\Linefeed))
+                sock))
 
 (defmacro with-http-output ((var raw &rest opts &key keep-alive (debug 0)
                              (return-code 200) (return-name "OK")

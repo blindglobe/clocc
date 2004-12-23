414cfeb99b6241838b19a1e1770b409a810c5730
sds <sds>
2004-12-23 19:11:21 +0000

    (matrix-to-file, matrix-from-file): start log with a FRESH-LINE

--- src/cllib/matrix.lisp
+++ src/cllib/matrix.lisp
@@ -13,7 +13,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: matrix.lisp,v 2.16 2004/12/23 16:50:36 sds Exp $
+;;; $Id: matrix.lisp,v 2.17 2004/12/23 19:11:21 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/matrix.lisp,v $
 
 (in-package :cllib)
@@ -69,7 +69,7 @@ By default prints the contents.
   "write matrix into the file, suitable for matlab/octave input"
   (with-timing (:out log)
     (when log
-      (format log ";; ~S: ~{~D~^x~}..." 'matrix-to-file
+      (format log "~&;; ~S: ~{~D~^x~}..." 'matrix-to-file
               (array-dimensions matrix))
       (force-output log))
     (with-open-file (out file :direction :output :if-exists :rename)
@@ -87,7 +87,7 @@ By default prints the contents.
   (with-timing (:out log)
     (with-open-file (in file :direction :input)
       (when log
-        (format log ";; ~S(~A, ~:D byte~:P)..."
+        (format log "~&;; ~S(~A, ~:D byte~:P)..."
                 'matrix-from-file file (file-length in))
         (force-output log))
       (let* ((list

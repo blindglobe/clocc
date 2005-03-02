d5b5e5845e4c7f287b9e1e672f9d8d9132e85e2c
sds <sds>
2005-03-02 22:00:28 +0000

    (test-xml): cl-ent now has 1604 entities

--- src/cllib/tests.lisp
+++ src/cllib/tests.lisp
@@ -1,10 +1,10 @@
 ;;; Regression Testing
 ;;;
-;;; Copyright (C) 1999-2004 by Sam Steingold
+;;; Copyright (C) 1999-2005 by Sam Steingold
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: tests.lisp,v 2.33 2005/01/27 23:02:46 sds Exp $
+;;; $Id: tests.lisp,v 2.34 2005/03/02 22:00:28 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/tests.lisp,v $
 
 (eval-when (load compile eval)
@@ -197,7 +197,7 @@
                  (incf num-err)))))
       (mesg :test out " ** ~s...~%" 'test-xml)
       (ts *xml-ent-file* 284)
-      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1208))
+      (ts (translate-logical-pathname "clocc:etc;cl-ent.xml") 1604))
     (mesg :test out " ** ~s: ~:d error~:p~2%" 'test-xml num-err)
     num-err))
 

d23caefc2d82788a2f88e999a45f02e451aab95a
airfoyle <airfoyle>
2005-05-04 14:25:44 +0000

    Keep ext.lisp from necessarily defining :port package with upper-case name

--- src/port/ext.lisp
+++ src/port/ext.lisp
@@ -8,22 +8,26 @@
 ;;; See <URL:http://www.gnu.org/copyleft/lesser.html>
 ;;; for details and the precise copyright document.
 ;;;
-;;; $Id: ext.lisp,v 1.38 2005/01/27 23:02:45 sds Exp $
+;;; $Id: ext.lisp,v 1.39 2005/05/04 14:25:44 airfoyle Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/port/ext.lisp,v $
 
-(defpackage "PORT"
-  (:use "COMMON-LISP")
-  (:nicknames "ORG.CONS.CLOCC/SDS/PORT")
-  (:export
-   "CODE" "CASE-ERROR" "NOT-IMPLEMENTED" ; conditions
-   "CODE-PROC" "CODE-MESG" "CODE-ARGS"   ; slot accessors
-   "DEFSUBST" "DEFCUSTOM" "DEFCONST"
-   "MK-ARR" "MAP-IN" "WITH-GENSYMS"
-   "GC" "QUIT"
-   "+EOF+" "EOF-P" "STRING-TOKENS" "REMOVE-PLIST"
-   #-cmu "REQUIRED-ARGUMENT"
-   "UNLOCK-PACKAGE" "RESTORE-PACKAGE-LOCK"
-   "COMPOSE" "COMPOSE-SAFE" "COMPOSE-F" "COMPOSE-ALL"))
+;;; Wrapped by Drew McDermott (so that some prior program can
+;;; define the :port package in Lisps that have lower-case symbols) --
+(eval-when (:compile-top-level :load-top-level :execute)
+   (cond ((not (find-package :port))
+	  (defpackage "PORT"
+	    (:use "COMMON-LISP")
+	    (:nicknames "ORG.CONS.CLOCC/SDS/PORT")
+	    (:export
+	     "CODE" "CASE-ERROR" "NOT-IMPLEMENTED" ; conditions
+	     "CODE-PROC" "CODE-MESG" "CODE-ARGS"   ; slot accessors
+	     "DEFSUBST" "DEFCUSTOM" "DEFCONST"
+	     "MK-ARR" "MAP-IN" "WITH-GENSYMS"
+	     "GC" "QUIT"
+	     "+EOF+" "EOF-P" "STRING-TOKENS" "REMOVE-PLIST"
+	     #-cmu "REQUIRED-ARGUMENT"
+	     "UNLOCK-PACKAGE" "RESTORE-PACKAGE-LOCK"
+	     "COMPOSE" "COMPOSE-SAFE" "COMPOSE-F" "COMPOSE-ALL")))))
 
 (in-package :port)
 

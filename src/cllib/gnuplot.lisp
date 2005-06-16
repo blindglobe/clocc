d770371ce01408d552db016d97b5006adcf75574
sds <sds>
2005-06-16 19:07:06 +0000

    (C) date update

--- src/cllib/gnuplot.lisp
+++ src/cllib/gnuplot.lisp
@@ -1,10 +1,10 @@
 ;;; Gnuplot (http://www.gnuplot.org/) interface
 ;;;
-;;; Copyright (C) 1997-2004 by Sam Steingold.
+;;; Copyright (C) 1997-2005 by Sam Steingold.
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: gnuplot.lisp,v 3.23 2005/06/16 19:05:15 sds Exp $
+;;; $Id: gnuplot.lisp,v 3.24 2005/06/16 19:07:06 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/gnuplot.lisp,v $
 
 ;;; the main entry point is WITH-PLOT-STREAM

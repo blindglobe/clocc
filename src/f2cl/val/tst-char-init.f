6e417fa16cff1424b50db57df66a6ab1e80feb92
rtoy <rtoy>
2006-01-04 17:55:10 +0000

    Initial version.

---
+++ src/f2cl/val/tst-char-init.f
@@ -0,0 +1,6 @@
+      CHARACTER*4 MODEL1(6), MODEL2(6)
+      DATA MODEL1/'    ','    ','    ','    ','  G ','  S '/,
+c     1     MODEL2
+      data model2(1)/' G  '/
+      data model2(2)/' S  '/
+      end

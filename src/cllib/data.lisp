399ed7d79901d9c3e3349da0c5a19bd97e0c0504
sds <sds>
2006-08-02 02:01:16 +0000

    (table): new defstruct (table-stats-refresh, column-histogram, add-column, plot-columns): added

--- src/cllib/data.lisp
+++ src/cllib/data.lisp
@@ -4,7 +4,7 @@
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: data.lisp,v 1.13 2006/07/26 17:05:18 sds Exp $
+;;; $Id: data.lisp,v 1.14 2006/08/02 02:01:16 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/data.lisp,v $
 
 (eval-when (compile load eval)
@@ -21,7 +21,9 @@
 (in-package :cllib)
 
 (export '(analyse-csv *buckets* *columns* evaluate-predictor
-          stat-column sc-pos sc-name sc-mdl sc-median sc-buckets))
+          stat-column sc-pos sc-name sc-mdl sc-median sc-buckets sc-table
+          table table-path table-lines table-stats table-names
+          table-stats-refresh column-histogram add-column plot-columns))
 
 (defcustom *buckets* (or null (cons lift:bucket)) ()
   "The list of buckets to fill in `analyse-csv'.")
@@ -99,34 +101,68 @@
       (values lines (- len drop)))))
 
 (defstruct (stat-column (:conc-name sc-))
+  (table nil :type (or nil table))
   (pos 0 :type index-t)
   (name "" :type string)
   (mdl +bad-mdl+ :type mdl)
   (buckets nil :type list)      ; of buckets
   (median nil :type (or null real)))
-
-(defun stat-column (lines col buckets names plot file
+(defmethod print-object ((sc stat-column) (out stream))
+  (if *print-readably* (call-next-method)
+      (print-unreadable-object (sc out :type t)
+        (format out "~W ~:D~@[ ~W~]" (sc-name sc) (sc-pos sc)
+                (let ((tab (sc-table sc))) (and tab (table-path tab)))))))
+(defun aref-i (i) (lambda (v) (aref v i)))
+(defun stat-column (lines col buckets names
                     &key (len (length lines)) (out *standard-output*)
-                    (max-name-length (max-name-length names)))
-  (let* ((name (column-name names col))
-         (key (lambda (v) (aref v col)))
-         (mdl (standard-deviation-mdl lines :key key)))
-    (mesg :log out "~3D ~V@A" col max-name-length name)
-    (if plot
-        (plot-histogram lines 100 :key key :plot plot :mdl mdl
-                        :xlabel (format nil "~A -- ~A(~D)" file name col))
-        (mesg :log out " ~A~%" mdl))
-    (let ((bl (mapcar (port:compose lift:bucket-empty lift:copy-bucket)
+                    (max-name-length (max-name-length names)) table)
+  (let* ((name (column-name names col)) (key (aref-i col))
+         (mdl (standard-deviation-mdl lines :key key))
+         (bl (mapcar (port:compose lift:bucket-empty lift:copy-bucket)
                       buckets)))
+    (mesg :log out "~3D ~V@A ~A~%" col max-name-length name mdl)
       (when buckets
         (lift:fill-buckets lines bl :key key)
         (dolist (b bl)
           (mesg :log out "  -  ~A  ~4F%~%"
                 b (/ (* 1d2 (lift:bucket-size b)) len))))
-      (make-stat-column :pos col :name name :mdl mdl :buckets bl))))
+    (make-stat-column :pos col :name name :mdl mdl :buckets bl :table table)))
+
+(defstruct table
+  (path "" :type (or string pathname)) ; file containing the table
+  (lines () :type (or list integer)) ; lines or line count
+  (stats () :type list)              ; of stat-column
+  (names #() :type vector))          ; of column names
+(defun table-lines$ (table)
+  (let ((lines (table-lines table)))
+    (etypecase lines
+      (integer lines)
+      (list (length lines)))))
+(defmethod print-object ((tab table) (out stream))
+  (if *print-readably* (call-next-method)
+      (print-unreadable-object (tab out :type t)
+        (format out "~W ~:Dx~:D" (table-path tab) (table-lines$ tab)
+                (length (table-names tab))))))
+(defvar *tables* nil "The list of currently loaded tables")
+(defun compress-tables ()
+  (dolist (tab *tables* (port:gc))
+    (etypecase (table-lines tab)
+      (integer (mesg :log t "~&no lines in <~S> - was ~:D~%"
+                     (table-path tab) (table-lines tab)))
+      (list (let ((len (length (table-lines tab))))
+              (mesg :log t "~&removed ~:D lines from <~S>~%"
+                    len (table-path tab))
+              (setf (table-lines tab) len))))))
+
+(defun column-histogram (sc nbins &rest plot-opts)
+  (apply #'plot-histogram (table-lines (sc-table sc)) nbins
+         :key (aref-i (sc-pos sc)) :mdl (sc-mdl sc)
+         :xlabel (format nil "~A -- ~A(~D)" (table-path (sc-table sc))
+                         (sc-name sc) (sc-pos sc))
+         plot-opts))
 
 ;;;###autoload
-(defun analyse-csv (file &key plot (first-line-names :default)
+(defun analyse-csv (file &key (first-line-names :default)
                     (out *standard-output*)
                     ((:columns *columns*) *columns*)
                     ((:buckets *buckets*) *buckets*))
@@ -140,18 +176,77 @@
             (if names
                 (reduce #'max columns :key (lambda (i) (length (aref names i)))
                         :initial-value *min-name-length*)
-                *min-name-length*)))
+                *min-name-length*))
+           (tab (make-table :path file :names names)))
       (assert columns (columns) "no interesting columns left")
-      (setf (values lines len)
+      (setf (values (table-lines tab) len)
             (strings-to-nums lines columns :names names
                              :max-name-length max-name-length
                              :len len :out out))
-      (values (mapcar (lambda (i)
-                        (stat-column lines i *buckets* names plot file
+      (push tab *tables*)
+      (setf (table-stats tab)
+            (mapcar (lambda (i)
+                      (stat-column (table-lines tab) i *buckets* names
                                      :max-name-length max-name-length
-                                     :len len :out out))
-                      columns)
-              lines names))))
+                                   :len len :out out :table tab))
+                    columns))
+      tab)))
+
+(defun table-stats-refresh (table)
+  "Update TABLE-STATS."
+  (let ((names (table-names table)) (stats (table-stats table)))
+    (map-into stats
+              (lambda (sc)
+                (stat-column (table-lines table) (sc-pos sc)
+                             (and stats (sc-buckets (car stats)))
+                             names :table table))
+              stats)))
+
+(defun add-column (table1 function name)
+  "Add a new column named NAME to the table.
+The value is computed by calling FUNCTION on the existing row vector.
+Everything is allocated anew."
+  (let* ((table2 (copy-table table1))
+         (names (table-names table2)) (len (length names))
+         (stats (table-stats table2)))
+    (setf (table-names table2) (concatenate 'vector names (vector name))
+          (table-lines table2)
+          (mapcar (lambda (v)
+                    (concatenate 'vector v (vector (funcall function v))))
+                  (table-lines table2))
+          (table-stats table2)
+          (nconc (mapcar (lambda (sc)
+                           (let ((cs (copy-stat-column sc)))
+                             (setf (sc-table cs) table2)
+                             cs))
+                         stats)
+                 (list (stat-column (table-lines table2) len
+                                    (and stats (sc-buckets (car stats)))
+                                    (table-names table2)
+                                    :table table2))))
+    (push table2 *tables*)
+    table2))
+
+(defun plot-columns (table col1 col2 &rest options)
+  "Plot one column vs the other."
+  (let ((names (table-names table)))
+    (flet ((to-col-name (x)
+             (etypecase x
+               (integer (values x (aref names x)))
+               (string (values (position x names :test #'string=) x))
+               (stat-column (values (sc-pos x) (sc-name x))))))
+      (multiple-value-bind (c1 n1) (to-col-name col1)
+        (multiple-value-bind (c2 n2) (to-col-name col2)
+          (apply #'plot-lists-arg
+                 (list (cons (format nil "~A vs ~A" n1 n2)
+                             (mapcar (lambda (v)
+                                       (cons (aref v c1) (aref v c2)))
+                                     (table-lines table))))
+                 (append options
+                         (list :xlabel n1 :ylabel n2
+                               :data-style :points
+                               :title (format nil "~A (~:D)" (table-path table)
+                                              (table-lines$ table))))))))))
 
 ;;;###autoload
 (defun evaluate-predictor (file &optional (out *standard-output*))

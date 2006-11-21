0e2b693a65832f87cfe4b324db086bed483194d6
rtoy <rtoy>
2006-11-21 21:35:40 +0000

    f2cl can't handle equivalences of different data types. Replace the implementation with a new Fortran version that doesn't use equivalences.

--- src/f2cl/packages/blas/dcabs1.f
+++ src/f2cl/packages/blas/dcabs1.f
@@ -1,8 +1,12 @@
       double precision function dcabs1(z)
-      double complex z,zz
-      double precision t(2)
-      equivalence (zz,t(1))
-      zz = z
-      dcabs1 = dabs(t(1)) + dabs(t(2))
+C ORIGINAL:
+c      double complex z,zz
+c      double precision t(2)
+c      equivalence (zz,t(1))
+c      zz = z
+c     dcabs1 = dabs(t(1)) + dabs(t(2))
+c NEW      
+      double complex z
+      dcabs1 = dabs(dble(z)) + dabs(dimag(z))
       return
       end

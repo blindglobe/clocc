1ea2cd2114f4871061ec220a63a8bcc1c7d17f8d
sds <sds>
2007-04-13 04:53:24 +0000

    (matrix-inverse): init logdet with 0, not 1

--- src/cllib/matrix.lisp
+++ src/cllib/matrix.lisp
@@ -9,11 +9,11 @@
 ;;; which relies on BLAS (http://www.netlib.org/blas) and
 ;;; LAPACK (http://www.netlib.org/lapack) for heavy-duty computations.
 ;;;
-;;; Copyright (C) 2000-2006 by Sam Steingold
+;;; Copyright (C) 2000-2007 by Sam Steingold
 ;;; This is Free Software, covered by the GNU GPL (v2)
 ;;; See http://www.gnu.org/copyleft/gpl.html
 ;;;
-;;; $Id: matrix.lisp,v 2.26 2006/06/22 20:11:01 sds Exp $
+;;; $Id: matrix.lisp,v 2.27 2007/04/13 04:53:24 sds Exp $
 ;;; $Source: /cvsroot/clocc/clocc/src/cllib/matrix.lisp,v $
 
 (eval-when (compile load eval)
@@ -489,7 +489,7 @@ If one of the principal minors of A is 0, `matrix-solve-lu'
   "Invert the matrix in-place.
 Return the log determinant of the matrix, parity, and the matrix argument."
   (let* ((dim (array-dimension mx 0))
-         (logdet 1) (parity 1)  ; log determinant & its sign
+         (logdet 0) (parity 1)  ; log determinant & its sign
          (l (make-array dim))   ; row permutation vector
          (m (make-array dim)))  ; column permutation vector
     (unless (and (= 2 (array-rank mx)) (= dim (array-dimension mx 1)))

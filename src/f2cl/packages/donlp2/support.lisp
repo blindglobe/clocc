1db89d672e17fa2ba134dca56996e343afb7f396
rtoy <rtoy>
2007-09-24 16:30:20 +0000

    Tell f2cl directly what the types of o8cpu, o8tida, ef, egradf, eh, egradh, eg, and egradg are.  This is necessary so that f2cl will compile the callers correctly.

--- src/f2cl/packages/donlp2/support.lisp
+++ src/f2cl/packages/donlp2/support.lisp
@@ -8,3 +8,58 @@
   (declare (ignore chan))
   ;; Print out time and date to channel chan
   (format-universal-time *standard-output* (get-universal-time)))
+
+;; Tell f2cl the function signatures
+(setf (gethash 'f2cl::o8cpu f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+       :arg-types '((f2cl::integer4))
+       :return-values '(nil)
+       :calls nil))
+
+(setf (gethash 'f2cl::o8tida f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+       :arg-types '((f2cl::integer4))
+       :return-values '(nil)
+       :calls nil))
+
+(setf (gethash 'f2cl::ef f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((array double-float (*))
+		      (double-float))
+	 :return-values '(nil f2cl::fx)
+	 :calls nil))
+(setf (gethash 'f2cl::egradf f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((array double-float (*))
+		      (array double-float (*)))
+	 :return-values '(nil nil)
+	 :calls nil))
+(setf (gethash 'f2cl::eh f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((f2cl::integer4)
+		      (array double-float (*))
+		      (double-float))
+	 :return-values '(nil nil nil)
+	 :calls nil))
+(setf (gethash 'f2cl::egradh f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((f2cl::integer4)
+		      (array double-float (*))
+		      (array double-float (*)))
+	 :return-values '(nil nil nil)
+	 :calls nil))
+(setf (gethash 'f2cl::eg f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((f2cl::integer4)
+		      (array double-float (*))
+		      (double-float))
+	 :return-values '(nil nil f2cl::gxi)
+	 :calls nil))
+(setf (gethash 'f2cl::egradg f2cl::*f2cl-function-info*)
+      (f2cl::make-f2cl-finfo
+	 :arg-types '((f2cl::integer4)
+		      (array double-float (*))
+		      (array double-float (*)))
+	 :return-values '(nil nil nil)
+	 :calls nil))
+

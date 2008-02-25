d419e1a0b5b89504d4f74d68bcaca5c0b8920180
rtoy <rtoy>
2008-02-25 17:11:43 +0000

    Updates to make hybrd test work.  The Lisp translation works.
    minpack.system:
    o Cleanup definition of logical pathname
    o Add some comments
    o Add new defsystem to compile hybrd test.  This test defines some
      functions and structures that are incompatible with lmdif test.
    
    minpack/Makefile
    o tst-hybrd uses a different input file.
    
    minpack/hybrd-ref.txt
    o Update with correct values.
    
    minpack/run-minpack-tests.lisp:
    o Add new function to run the hybrd test.  (Should add hybrj test
      too.)

--- src/f2cl/packages/minpack/run-minpack-tests.lisp
+++ src/f2cl/packages/minpack/run-minpack-tests.lisp
@@ -22,9 +22,30 @@
 		      (funcall f))
 		 (setf (gethash 5 f2cl-lib::*lun-hash*) old-in-lun)
 		 (setf (gethash 6 f2cl-lib::*lun-hash*) old-out-lun)))))))
-    ;; thybrd doesn't seem to work.  Haven't investigated.
-    (dolist (f '(tlmdif tlmder #+nil thybrd))
+    (dolist (f '(tlmdif tlmder))
       (run-test "minpack:lmdif-input.dat" f))))
+
+(defun run-minpack-test-hybrd ()
+  (flet
+      ((run-test (input-file f)
+	 (with-open-file (input input-file :direction :input)
+	   (with-open-file (output (make-pathname :host "minpack"
+						  :name (string-downcase (string f))
+						  :type "txt")
+				   :direction :output
+				   :if-exists :supersede)
+	     (let ((old-in-lun (gethash 5 f2cl-lib::*lun-hash*))
+		   (old-out-lun (gethash 6 f2cl-lib::*lun-hash*)))
+	       (unwind-protect
+		    (progn
+		      (setf (gethash 5 f2cl-lib::*lun-hash*) input)
+		      (setf (gethash 6 f2cl-lib::*lun-hash*)
+			    (make-broadcast-stream output *standard-output*))
+		      (funcall f))
+		 (setf (gethash 5 f2cl-lib::*lun-hash*) old-in-lun)
+		 (setf (gethash 6 f2cl-lib::*lun-hash*) old-out-lun)))))))
+    (dolist (f '(thybrd))
+      (run-test "minpack:hybrd-input.dat" f))))
   
 
   

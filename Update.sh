eecf712f1f332708430e9c410c3a0f7382ed4681
AJ Rossini <blindglobe@gmail.com>
2010-03-23 11:48:29 +0100

    update git repo from cvs sourceforge

---
+++ Update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This repository was initialized through cvs login to sourceforge, and then...
+
+git cvsimport -v -o cvs -d :pserver:anonymous@clocc.cvs.sourceforge.net:/cvsroot/clocc clocc
+
+
